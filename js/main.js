4785c57cb7419a45c1164af1fdb0abae975b996c
jacobtony <jacobtony994@gmail.com>
2020-02-17 16:24:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if(navigator.serviceWorker){
     
     window.addEventListener("load", ()=>{
         navigator.serviceWorker
-        .register("https://raw.githack.com/jacobtony/service-worker/master/sw_cached_pages.js")
+        .register("sw_cached_pages.js")
         .then(reg => console.log(reg));
     })
 }
