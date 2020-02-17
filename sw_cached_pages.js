94ae6dce27841ff38a0bf3a2c953e8bf469d9076
Jacob Tony <jacobtony@brambles.com>
2020-02-17 16:06:43 +0530

    first commit

---
+++ sw_cached_pages.js
@@ -0,0 +1,46 @@
+const cacheName = "v3";
+const cacheAssets = [
+    "/index.html",
+    "/about.html",
+    "/js/main.js",
+    "/css/style.css"
+]
+//Call install Event
+self.addEventListener("install", (e)=>{
+    console.log("SW Installed");
+});
+self.addEventListener("activate", (e)=>{
+    console.log("SW Acivated");
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(cacheNames.map(cache => {
+                if(cache !== cacheName){
+                    caches.delete(cache)
+                }
+            }))
+        }
+
+    ))
+    
+})
+
+self.addEventListener("fetch", (e) => {
+    console.log(e.request)
+    e.respondWith(
+        fetch(e.request).then((res)=>{
+            const resClone = res.clone();
+            console.log("Fetching success")  
+            caches
+            .open(cacheName)
+            .then(cache => {
+               cache.put(e.request, resClone);
+
+            })
+            return res
+        })
+        .catch(() => {
+            console.log("Fetching error")
+            return caches.match(e.request)
+        })
+    )
+})
