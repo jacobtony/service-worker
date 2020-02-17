77fd8107c6b1a937360dfca66cfd20e59d5fb8aa
Jacob Tony <jacobtony@brambles.com>
2020-02-17 16:31:44 +0530

    bug fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if(navigator.serviceWorker){
     
     window.addEventListener("load", ()=>{
         navigator.serviceWorker
-        .register("/sw_cached_pages.js")
+        .register("sw_cached_pages.js")
         .then(reg => console.log(reg));
     })
 }
