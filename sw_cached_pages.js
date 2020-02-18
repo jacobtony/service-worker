f88da768010767dd7a1e4e57c6b031ef6bbc7ef9
jacobtony <jacobtony994@gmail.com>
2020-02-18 09:21:24 +0530

    Update sw_cached_pages.js

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -36,6 +36,7 @@ self.addEventListener("fetch", (e) => {
                cache.put(e.request, resClone);
 
             })
+            console.log(res)
             return res
         })
         .catch(() => {
